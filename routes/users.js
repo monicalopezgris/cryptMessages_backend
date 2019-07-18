c827e3387e426f00ccf1b644aa7de0210de3cf88
monicalopezgris <monicalopezgris@gmail.com>
2019-07-18 10:47:00 +0200

    ADD login route

--- routes/users.js
+++ routes/users.js
@@ -4,36 +4,59 @@ const bcrypt = require('bcrypt');
 const User = require('../models/users')
 const router = express.Router();
 
-/* POST signup */
-router.post('/', async (req, res, next) => {
-  const { username, password } = req.body;
-  try {
-    const isUser = await User.findOne({ username });
-    if (isUser) {
-      next(createError(422));
-    } else {
-      var salt = bcrypt.genSaltSync(10);
-      var hash = bcrypt.hashSync(password, salt);
-      const user = await User.create({
-        username,
-        password: hash,
-      });
-      const { _id: id } = user;
-      req.session.currentUser = id;
-      res.status(200).json(user);
+router.post(
+  '/signup',
+  async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+      const isUser = await User.findOne({ username });
+      if (isUser) {
+        next(createError(422));
+      } else {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(password, salt);
+        const user = await User.create({
+          username,
+          password: hash,
+        });
+        const { _id: id } = user;
+        req.session.currentUser = id;
+        res.status(200).json(user);
+      }
+    } catch (error) {
+      next(createError(404));
     }
-  } catch (error) {
-    next(createError(404));
-  }
-});
+  });
 
-router.post('/logout', (req, res, next) => {
-  try {
-    req.session.destroy();
-    return res.status(204).send();
-  } catch (error) {
-    next(createError(404))
-  }
-});
+router.post(
+  '/login',
+  async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        next(createError(404));
+      } else if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        return res.status(200).json(user);
+      } else {
+        next(createError(401));
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.post(
+  '/logout',
+  (req, res, next) => {
+    try {
+      req.session.destroy();
+      return res.status(204).send();
+    } catch (error) {
+      next(createError(404))
+    }
+  });
 
 module.exports = router;
