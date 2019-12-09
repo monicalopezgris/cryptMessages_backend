cdff65df63a20c1dc4dfced5918512c149bc963d
monicalopezgris <monicalopezgris@gmail.com>
2019-12-09 19:35:53 +0100

    ADD encrypt / decrypt test

--- test/encrypt.js
+++ test/encrypt.js
@@ -4,17 +4,29 @@ const assert = require('assert');
 const crypt = require('../helpers/crypt');
 
 describe('Encryptation tests', () => {
-  describe('isAlfa function', () => {
-    const testTrue = 'b';
-    const testFalse = '*';
+  describe('encrypt function', () => {
+    const jump = 2;
     it('should not return undefined', () => {
-      assert.notEqual(undefined, crypt.isAlpha(testTrue, /^[a-z]+$/))
+      const testArray = ['b'];
+      assert.notEqual(undefined, crypt.encrypt(testArray, jump))
     })
-    it('should return true if input is a letter', () => {
-      assert.equal(true, crypt.isAlpha(testTrue, /^[a-z]+$/))
+    it('should return a d on a 2 jump encryptation on b', () => {
+      const testArray = ['b'];
+      assert.equal('d', crypt.encrypt(testArray, jump))
     })
-    it('should return false if input is not a letter', () => {
-      assert.equal(false, crypt.isAlpha(testFalse, /^[a-z]+$/))
+  })
+})
+
+describe('Decryptation tests', () => {
+  describe('decrypt function', () => {
+    const jump = 2;
+    it('should not return undefined', () => {
+      const testArray = ['d', 'q', 'j'];
+      assert.notEqual(undefined, crypt.decrypt(testArray, jump))
+    })
+    it("should decrypt 'd' as 'b' on a 2 jump decryptation", () => {
+      const testArray = ['d'];
+      assert.equal('b', crypt.decrypt(testArray, jump))
     })
   })
 })
