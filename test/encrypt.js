e19b9e048a43f5fc7ca658d9041e157bee12a3b9
monicalopezgris <monicalopezgris@gmail.com>
2019-12-14 00:00:32 +0100

    fix chai-html

--- test/encrypt.js
+++ test/encrypt.js
@@ -1,46 +1,53 @@
 /* eslint-disable */
 
 const assert = require('assert');
+const crypt = require('../helpers/crypt');
 const chai = require('chai');
 const expect = require('chai').expect;
-const crypt = require('../helpers/crypt');
-
-describe('Encryptation tests', () => {
-  describe('encrypt function', () => {
-    const jump = 2;
-    it('should not return undefined', () => {
-      const testArray = ['b'];
-      assert.notEqual(undefined, crypt.encrypt(testArray, jump))
-    })
-    it('should return a d on a 2 jump encryptation on b', () => {
-      const testArray = ['b'];
-      assert.equal('d', crypt.encrypt(testArray, jump))
-    })
-  })
-})
-
-describe('Decryptation tests', () => {
-  describe('decrypt function', () => {
-    const jump = 2;
-    it('should not return undefined', () => {
-      const testArray = ['d', 'q', 'j'];
-      assert.notEqual(undefined, crypt.decrypt(testArray, jump))
-    })
-    it("should decrypt 'd' as 'b' on a 2 jump decryptation", () => {
-      const testArray = ['d'];
-      assert.equal('b', crypt.decrypt(testArray, jump))
-    })
-  })
-})
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+const url = 'http://localhost:5000';
 
 describe('Test login ', () => {
-  it('should return a json user', () => {
-    chai.request(url)
-      .post('/login')
-      .send({ username: 'admin', password: 'admin' })
-      .end(function (err, res) {
-        console.log(res.body)
+  const userCredentials = { username: 'admin', password: 'admin' }
+  it('should return a json user', (done) => {
+    chai
+      .request(url)
+      .post('/api/auth/login')
+      .send(userCredentials)
+      .end((err, res) => {
+        console.log('console.log=>', res, 'endOfConsolelog')
         expect(res).to.have.status(200);
       });
+    done();
   });
 });
+
+// describe('Encryptation tests', () => {
+//   describe('encrypt function', () => {
+//     const jump = 2;
+//     it('should not return undefined', () => {
+//       const testArray = ['h', 'o', 'l', 'a'];
+//       assert.notEqual(undefined, crypt.encrypt(testArray, jump))
+//     })
+//     it('should return a d on a 2 jump encryptation on b', () => {
+//       const testArray = ['h', 'o', 'l', 'a'];
+//       assert.equal('jqnc', crypt.encrypt(testArray, jump))
+//     })
+//   })
+// })
+
+// describe('Decryptation tests', () => {
+//   describe('decrypt function', () => {
+//     const jump = 2;
+//     it('should not return undefined', () => {
+//       const testArray = ['d', 'q', 'j'];
+//       assert.notEqual(undefined, crypt.decrypt(testArray, jump))
+//     })
+//     it("should decrypt 'd' as 'b' on a 2 jump decryptation", () => {
+//       const testArray = ['d'];
+//       assert.equal('b', crypt.decrypt(testArray, jump))
+//     })
+//   })
+// })
+
