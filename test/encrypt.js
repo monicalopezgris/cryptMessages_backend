d23314e43cede4f436a575cd992a14ac762b92c4
monicalopezgris <monicalopezgris@gmail.com>
2019-12-15 23:21:56 +0100

    ADD testing logout test

--- test/encrypt.js
+++ test/encrypt.js
@@ -11,11 +11,21 @@ const url = 'http://localhost:5000/api';
 describe('Test #auth ', () => {
   const userCredentials = { username: 'admin', password: 'admin' }
 
-  it('should return a json user', (done) => {
+  it('should return 200 on LOGIN', (done) => {
     chai
       .request(url)
       .post('/auth/login')
       .send(userCredentials)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+      });
+    done();
+  });
+
+  it('should return 200 on LOGOUT', (done) => {
+    chai
+      .request(url)
+      .post('/auth/logout')
       .end((err, res) => {
         expect(res).to.have.status(200);
       });
